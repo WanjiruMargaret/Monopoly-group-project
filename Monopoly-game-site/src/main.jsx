0d4674f620c8b35017493f780b7984952c3f2d5b
WayneMuongi <waynemuongi77@gmail.com>
2025-07-22 14:30:12 +0300

    landing page

--- Monopoly-game-site/src/main.jsx
+++ Monopoly-game-site/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './GameStartScreen.css'
 
-import App from './App'
-// import Routing from './GameBoard/Route'
+
+
+import GameStartScreen from './GameBoard/StartScreen'
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <App />
+    <GameStartScreen />
   </StrictMode>
 )
